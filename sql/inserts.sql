61f75031c0d40de2a16487d9b546758590255e3b
Maria Polakowska <01169227@pw.edu.pl>
2023-12-31 01:00:08 +0100

    P5K-73 currency inserts

---
+++ sql/inserts.sql
@@ -0,0 +1,12 @@
+INSERT INTO `currency` (`currency_id`, `name`, `abbreviation`, `account_number`) VALUES
+(1, 'Dolar Amerykański', 'USD', '12345678901234567890123456'),
+(2, 'Euro', 'EUR', '98765432109876543210987654'),
+(3, 'Funt Brytyjski', 'GBP', '55552222333344446666777788'),
+(4, 'Jen Japoński', 'JPY', '99998888777766665555444433'),
+(5, 'Dolar Kanadyjski', 'CAD', '77776666555544443333222211'),
+(6, 'Dolar Australijski', 'AUD', '12341234123412341234123412'),
+(7, 'Frank Szwajcarski', 'CHF', '99996666333311112222444499'),
+(8, 'Juan Chiński', 'CNY', '88887777666655554444333322'),
+(9, 'Real Brazylijski', 'BRL', '77778888999911112222333344'),
+(10, 'Rupia Indyjska', 'INR', '55554444333322221111000066'),
+(11, 'Złoty Polski', 'PLN', '11112222333344445555666677');

8b2b80dbc0bfe89e85c781a1daeee837d1bb57a7
mczech1 <magxczech@gmail.com>
2024-01-08 22:56:33 +0100

    again, docker change

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -9,9 +9,10 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "localhost:3000",
-    "http://localhost:3307",
-    "localhost:3307",
 ]
+
+#    "http://localhost:3307",
+#    "localhost:3307",
 
 # CORSMiddleware => cross-origin requests
 # eg. requests that originate from a different protocol, IP address, domain name, or port

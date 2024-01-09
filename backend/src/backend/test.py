c594aa5f5ef9c45f05d935dfd51f16a4c966f126
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-01-09 16:11:46 +0100

    P5K-65 test get_user_data

--- backend/src/backend/test.py
+++ backend/src/backend/test.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_mock import mocker
 from backend.database import login
+import backend.database as db
 
 def test_login_successful(mocker):
     # Mock the cursor and execute method
@@ -36,3 +37,39 @@ def test_login_database_error(mocker):
 
     with pytest.raises(Exception, match='Database error'):
         login('test@example.com', 'password123')
+
+
+def test_user_data_correct_id(mocker):
+    # Mock the cursor and execute method
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.fetchone.return_value = ('Test name', 'Test surname', 'test@example.com')
+
+    name, surname, email = db.get_user_data(1)
+
+    assert name == 'Test name'
+    assert surname == 'Test surname'
+    assert email == 'test@example.com'
+
+
+def test_user_data_incorrect_id(mocker):
+    # Mock the cursor and execute method
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.fetchone.return_value = ()
+
+    name, surname, email = db.get_user_data(1)
+
+    assert name == None
+    assert surname == None
+    assert email == None
+
+
+def test_user_data_database_error(mocker):
+    # Mock the cursor and execute method to raise an exception
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.execute.side_effect = Exception('Database error')
+
+    with pytest.raises(Exception, match='Database error'):
+        db.get_user_data(1)

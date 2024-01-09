23175e63509cc0dd7994f41d8d65c79dff6e6473
Maria Polakowska <01169227@pw.edu.pl>
2024-01-09 20:40:22 +0100

    correct badabase

--- sql/user_registration.sql
+++ sql/user_registration.sql
@@ -5,7 +5,7 @@ DELIMITER $$
 USE `kantor`$$
 CREATE PROCEDURE `user_registration`(u_password varchar(100), u_name varchar(45), u_surname varchar(45), u_email varchar(45))
 BEGIN
-INSERT INTO user (password, name, surname, email)
+INSERT INTO user (password_hash, name, surname, email)
 VALUE (u_password, u_name, u_surname, u_email);
 END$$
 

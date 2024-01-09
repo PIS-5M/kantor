a9356671b8fc24ea089864e99e436e140881fc7f
Inekera1 <95167426+Inekera1@users.noreply.github.com>
2024-01-09 20:48:59 +0100

    Update create_packages.sh

--- create_packages.sh
+++ create_packages.sh
@@ -9,7 +9,7 @@ fi
 
 # Uruchom skrypt w podkatalogu ./backend
 echo "Uruchamiam skrypt do tworzenia paczki backendu w podkatalogu./backend..."
-(cd ./backend && chmod +x create_python_package.sh && ./create_python_package.sh "$PARAMETER")
+(cd /var/jenkins_home/workspace/kantor5M_main/backend && chmod +x create_python_package.sh && ./create_python_package.sh "$PARAMETER")
 
 # Uruchom skrypt w podkatalogu ./app
 echo "Uruchamiam skrypt do tworzenia paczki frontendu w podkatalogu ./app..."

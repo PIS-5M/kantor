72cf72265aeb9f4335da9c2ac382cc2e72596e52
Maria Polakowska <01169227@pw.edu.pl>
2024-01-09 22:28:03 +0100

    P5K-44 add some tests

--- backend/src/backend/database.py
+++ backend/src/backend/database.py
@@ -2,7 +2,7 @@ import mysql.connector
 import bcrypt
 import math
 
-# Ustawienia połączenia
+# Ustawienia polaczenia
 db_config = {
     "host": "0.0.0.0",
     "user": "root",
@@ -14,14 +14,14 @@ db_config = {
 def registration(pass_hash, name, surname, mail):
     conn = mysql.connector.connect(**db_config)
     try:
-        # Utwórz obiekt kursora
+        # Utworz obiekt kursora
         cursor = conn.cursor()
         cursor.callproc("user_registration", (pass_hash, name, surname, mail))
         cursor.execute("COMMIT;")
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if "cursor" in locals() and cursor is not None:
             cursor.close()
         if "conn" in locals() and conn.is_connected():
@@ -31,9 +31,9 @@ def registration(pass_hash, name, surname, mail):
 def login(email, password):
     conn = mysql.connector.connect(**db_config)
     try:
-        # Utwórz obiekt kursora
+        # Utworz obiekt kursora
         cursor = conn.cursor()
-        # Wywołaj funkcję
+        # Wywolaj funkcje
         args = (email,)
         cursor.execute(f"SELECT password, user_id from user where email=%s", args)
 
@@ -44,12 +44,12 @@ def login(email, password):
         ):
             return result[1]
         else:
-            # złe hasło
+            # zle haslo
             return None
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if "cursor" in locals() and cursor is not None:
             cursor.close()
         if "conn" in locals() and conn.is_connected():
@@ -59,9 +59,9 @@ def login(email, password):
 def email_used(email):
     conn = mysql.connector.connect(**db_config)
     try:
-        # Utwórz obiekt kursora
+        # Utworz obiekt kursora
         cursor = conn.cursor()
-        # Wywołaj funkcję
+        # Wywolaj funkcje
         args = (email,)
         cursor.execute(f"SELECT user_id from user where email=%s", args)
 
@@ -70,12 +70,12 @@ def email_used(email):
         if result:
             return True
         else:
-            # email nie jest zajęty
+            # email nie jest zajety
             return False
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if "cursor" in locals() and cursor is not None:
             cursor.close()
         if "conn" in locals() and conn.is_connected():
@@ -85,18 +85,18 @@ def email_used(email):
 def get_all_currency():
     conn = mysql.connector.connect(**db_config)
     try:
-        # Utwórz obiekt kursora
+        # Utworz obiekt kursora
         cursor = conn.cursor()
-        # Wywołaj funkcję
+        # Wywolaj funkcje
         cursor.execute(f"select * from currency;")
 
         # Pobierz wyniki
         currency = cursor.fetchall()
         return currency
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if "cursor" in locals() and cursor is not None:
             cursor.close()
         if "conn" in locals() and conn.is_connected():
@@ -111,26 +111,22 @@ def new_offer(user_id, selled_currency_id, value, wanted_currency_id, exchange_r
     offer_match(offer_id, selled_currency_id, value, wanted_currency_id, reverse_exchange_rate)
 
 def offer_match(offer_id, selled_currency_id, value, wanted_currency_id, exchange_rate):
-    offer_list = get_offer_list(selled_currency_id, wanted_currency_id, exchange_rate)
-    print(offer_list)
+    offer_list = get_possible_match_offer_list(selled_currency_id, wanted_currency_id, exchange_rate)
     remaining_value = value
     match_list = []
     while remaining_value > 0 and offer_list:
         best_offer = min(offer_list, key=lambda x: (x[7], x[2]))
         offer_list.remove(best_offer)
         match_wanted_money = math.floor(best_offer[5] * best_offer[7] * 100 ) / 100
-        print((best_offer[0], best_offer[5], offer_id, match_wanted_money))
         if match_wanted_money <= remaining_value:
-            print(1)
             add_match_offers(best_offer[0], best_offer[5], offer_id, match_wanted_money)
             remaining_value -= match_wanted_money
-            match_list.append([match_wanted_money, best_offer[5]]) # [kwota sprzedana (ile chce zmaczowany), kwota kupiona(ile wystawił zmaczowany)]
+            match_list.append([match_wanted_money, best_offer[5]]) # [kwota sprzedana (ile chce zmaczowany), kwota kupiona(ile wystawil zmaczowany)]
         else:
-            print(2)
             reverse_exchange_rate = math.floor(1 / exchange_rate * 100) / 100
             match_money = math.floor(reverse_exchange_rate * remaining_value * 100) / 100
             add_match_offers(best_offer[0], match_money, offer_id, remaining_value)
-            match_list.append([remaining_value, match_money]) # [kwota sprzedana (ile mam zostało), kwota kupiona (ile płaci zmaczowany)]
+            match_list.append([remaining_value, match_money]) # [kwota sprzedana (ile mam zostalo), kwota kupiona (ile placi zmaczowany)]
             remaining_value = 0
     return match_list
 
@@ -141,18 +137,18 @@ def add_match_offers(earlier_offer_id, earlier_value, later_offer_id, later_valu
         cursor.callproc('match_offers', (earlier_offer_id, later_offer_id, earlier_value, later_value))
         conn.commit()
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if 'cursor' in locals() and cursor is not None:
             cursor.close()
         if 'conn' in locals() and conn.is_connected():
             conn.close()
 
-def get_offer_list(selled_currency_id, wanted_currency_id, exchange_rate):
+def get_possible_match_offer_list(selled_currency_id, wanted_currency_id, exchange_rate):
     conn = mysql.connector.connect(**db_config)
     try:
-        # Utwórz obiekt kursora
+        # Utworz obiekt kursora
         cursor = conn.cursor()
         # Pobierz wynik z procedury
         args = (wanted_currency_id, selled_currency_id, exchange_rate)
@@ -160,9 +156,9 @@ def get_offer_list(selled_currency_id, wanted_currency_id, exchange_rate):
         result = cursor.fetchall()
         return result
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if 'cursor' in locals() and cursor is not None:
             cursor.close()
         if 'conn' in locals() and conn.is_connected():
@@ -172,7 +168,7 @@ def get_offer_list(selled_currency_id, wanted_currency_id, exchange_rate):
 def add_new_offer(user_id, selled_currency_id, value, wanted_currency_id, exchange_rate):
     conn = mysql.connector.connect(**db_config)
     try:
-        # Utwórz obiekt kursora
+        # Utworz obiekt kursora
         cursor = conn.cursor()
         cursor.callproc('add_offer', (user_id, selled_currency_id, value, wanted_currency_id, exchange_rate))
 
@@ -182,9 +178,9 @@ def add_new_offer(user_id, selled_currency_id, value, wanted_currency_id, exchan
         conn.commit()
         return result[0]
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if 'cursor' in locals() and cursor is not None:
             cursor.close()
         if 'conn' in locals() and conn.is_connected():
@@ -193,14 +189,14 @@ def add_new_offer(user_id, selled_currency_id, value, wanted_currency_id, exchan
 def add_internal_transaction(user_id, currency_id, value):
     conn = mysql.connector.connect(**db_config)
     try:
-        # Utwórz obiekt kursora
+        # Utworz obiekt kursora
         cursor = conn.cursor()
         cursor.callproc('add_internal_transaction', (user_id, currency_id, value))
         conn.commit()
     except mysql.connector.Error as err:
-        print(f"Błąd: {err}")
+        print(f"Blad: {err}")
     finally:
-        # Zamknij kursor i połączenie
+        # Zamknij kursor i polaczenie
         if 'cursor' in locals() and cursor is not None:
             cursor.close()
         if 'conn' in locals() and conn.is_connected():
--- backend/src/backend/test.py
+++ backend/src/backend/test.py
@@ -1,7 +1,7 @@
 import pytest
 from pytest_mock import mocker
 from unittest.mock import patch
-from backend.database import login, add_new_offer, add_internal_transaction
+import backend.database as db
 import mysql.connector
 
 def test_login_successful(mocker):
@@ -13,7 +13,7 @@ def test_login_successful(mocker):
     # Mock bcrypt.checkpw to always return True
     mocker.patch('backend.database.bcrypt.checkpw', return_value=True)
 
-    result = login('test@example.com', 'password123')
+    result = db.login('test@example.com', 'password123')
 
     assert result == 1
 
@@ -26,7 +26,7 @@ def test_login_failed(mocker):
     # Mock bcrypt.checkpw to always return False
     mocker.patch('backend.database.bcrypt.checkpw', return_value=False)
 
-    result = login('test@example.com', 'wrong_password')
+    result = db.login('test@example.com', 'wrong_password')
 
     assert result is None
 
@@ -37,7 +37,7 @@ def test_login_database_error(mocker):
     mock_cursor.execute.side_effect = Exception('Database error')
 
     with pytest.raises(Exception, match='Database error'):
-        login('test@example.com', 'password123')
+        db.login('test@example.com', 'password123')
 
 
 # add offer
@@ -50,43 +50,39 @@ def test_add_new_offer():
     exchange_rate = 4.5
 
     with patch("backend.database.mysql.connector.connect") as mock_connect:
-        # Ustawienie symulowanych wyników
-        mock_cursor = mock_connect.return_value.cursor.return_value
-        mock_cursor.fetchone.return_value = (123,)  # Symulowany wynik z procedury
 
-        # Wywołanie funkcji i sprawdzenie wyniku
-        result = add_new_offer(user_id, selled_currency_id, value, wanted_currency_id, exchange_rate)
+        mock_cursor = mock_connect.return_value.cursor.return_value
+        mock_cursor.fetchone.return_value = (123,)
+
+        result = db.add_new_offer(user_id, selled_currency_id, value, wanted_currency_id, exchange_rate)
 
         assert result == 123
-        # Sprawdzenie, czy metoda cursor.callproc została wywołana z odpowiednimi parametrami
+
         mock_cursor.callproc.assert_called_once_with(
             'add_offer', (user_id, selled_currency_id, value, wanted_currency_id, exchange_rate)
         )
-        # Sprawdzenie, czy metoda cursor.execute została wywołana z odpowiednim zapytaniem
+
         mock_cursor.execute.assert_called_once_with("SELECT LAST_INSERT_ID()")
-        # Sprawdzenie, czy metoda commit została wywołana
+
         mock_connect.return_value.commit.assert_called_once()
 
-    # Sprawdzenie, czy metoda close została wywołana na połączeniu
     mock_connect.return_value.close.assert_called_once()
 
 def test_add_new_offer_error(mocker):
     mocker.patch('backend.database.mysql.connector.connect')
     mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
     mock_cursor.execute.side_effect = Exception('Database error')
-        # Wywołanie funkcji i sprawdzenie, czy oczekiwany wyjątek został rzucony
+
     with pytest.raises(Exception, match='Database error'):
-        add_new_offer(1, 2, 100.0, 3, 4.5)
+        db.add_new_offer(1, 2, 100.0, 3, 4.5)
 
 def test_add_internal_transaction_success():
     with patch("backend.database.mysql.connector.connect") as mock_connect:
-        # Ustawienie symulowanych wyników
+
         mock_cursor = mock_connect.return_value.cursor.return_value
 
-        # Wywołanie funkcji
-        add_internal_transaction(1, 2, 100.0)
+        db.add_internal_transaction(1, 2, 100.0)
 
-        # Sprawdzenie, czy metody zostały poprawnie wywołane
         mock_cursor.callproc.assert_called_once_with('add_internal_transaction', (1, 2, 100.0))
         mock_connect.return_value.commit.assert_called_once()
 
@@ -96,7 +92,48 @@ def test_add_internal_transaction_success():
 #         mock_cursor = mock_connect.return_value.cursor.return_value
 #         mock_cursor.callproc.side_effect = mysql.connector.Error('Database error')
 
-#         # Wywołanie funkcji i sprawdzenie, czy oczekiwany wyjątek został rzucony
 #         with pytest.raises(Exception):
 #             add_internal_transaction(1, 2, 100.0)
 
+def test_get_all_currency_success(mocker):
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+
+    mock_cursor.fetchall.return_value = [(1, 'USD'), (2, 'EUR'), (3, 'GBP')]
+
+    result = db.get_all_currency()
+
+    assert result == [(1, 'USD'), (2, 'EUR'), (3, 'GBP')]
+
+def test_get_all_currency_exception(mocker):
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.execute.side_effect = Exception('Some other error')
+
+    with pytest.raises(Exception, match='Some other error'):
+        db.get_all_currency()
+
+
+def test_add_new_offer_success(mocker):
+    # Mockowanie obiektu kursora i połączenia
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+
+    # Symulowanie wyniku procedury LAST_INSERT_ID()
+    mock_cursor.fetchone.return_value = (1,)
+
+    # Wywołanie funkcji
+    result = db.add_new_offer(1, 2, 100.0, 3, 1.5)
+
+    # Sprawdzenie, czy funkcja zwraca oczekiwany wynik
+    assert result == 1
+
+def test_add_new_offer_exception(mocker):
+    # Mockowanie obiektu kursora i połączenia z ogólnym wyjątkiem
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.execute.side_effect = Exception('Some other error')
+
+    # Wywołanie funkcji i sprawdzenie, czy oczekiwany wyjątek został rzucony
+    with pytest.raises(Exception, match='Some other error'):
+        db.add_new_offer(1, 2, 100.0, 3, 1.5)

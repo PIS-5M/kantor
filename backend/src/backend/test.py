8de85b5dabc9b465042036c271b20e526f824fea
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-01-10 04:40:02 +0100

    P5K-90 tests

--- backend/src/backend/test.py
+++ backend/src/backend/test.py
@@ -1,6 +1,8 @@
 import pytest
 from pytest_mock import mocker
 from backend.database import login
+import backend.database as db
+from unittest.mock import patch, MagicMock
 
 def test_login_successful(mocker):
     # Mock the cursor and execute method
@@ -36,3 +38,64 @@ def test_login_database_error(mocker):
 
     with pytest.raises(Exception, match='Database error'):
         login('test@example.com', 'password123')
+
+
+
+def test_wallet_add(mocker):
+    # Mock the cursor and execute method
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.fetchone.return_value = ()
+    wallet_id = 1
+    value = 100.0
+    db.wallet_add(wallet_id, value)
+
+def test_wallet_add_error(mocker):
+    # Mock the cursor and execute method to raise an exception
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.execute.side_effect = Exception('Database error')
+    wallet_id = 1
+    value = 100.0
+
+    with pytest.raises(Exception, match='Database error'):
+        db.wallet_add(wallet_id, value)
+
+
+def test_wallet_subtract(mocker):
+    # Mock the cursor and execute method
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.fetchone.return_value = (200.0,)
+    wallet_id = 1
+    value = 50.0
+
+    result = db.wallet_subtract(wallet_id, value)
+
+    assert result is True
+
+
+
+def test_wallet_subtract_to_little(mocker):
+    # Mock the cursor and execute method
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.fetchone.return_value = (50.0,)
+    wallet_id = 1
+    value = 200.0
+
+    result = db.wallet_subtract(wallet_id, value)
+
+    assert result is False
+
+
+def test_wallet_subtract_error(mocker):
+    # Mock the cursor and execute method to raise an exception
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.execute.side_effect = Exception('Database error')
+    wallet_id = 1
+    value = 100.0
+
+    with pytest.raises(Exception, match='Database error'):
+        db.wallet_subtract(wallet_id, value)

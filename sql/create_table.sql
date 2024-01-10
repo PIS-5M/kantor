4cd198db978f09db7eda86464f55250a55fa02b7
Maria Polakowska <01169227@pw.edu.pl>
2024-01-10 07:27:02 +0100

    add user_registration

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -111,3 +111,17 @@ LEFT JOIN
     internal_transactions it ON w.wallet_id = it.wallet_id
 GROUP BY
     w.wallet_id, w.user_id;
+
+
+USE `kantor`;
+DROP procedure IF EXISTS `user_registration`;
+
+DELIMITER $$
+USE `kantor`$$
+CREATE PROCEDURE `user_registration`(u_password varchar(100), u_name varchar(45), u_surname varchar(45), u_email varchar(45))
+BEGIN
+INSERT INTO user (password_hash, name, surname, email)
+VALUE (u_password, u_name, u_surname, u_email);
+END$$
+
+DELIMITER ;

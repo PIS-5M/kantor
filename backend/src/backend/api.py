cb1357a8eefa1377fc7d58568c211775f1e7b23e
goshakowska <margo.ko@wp.pl>
2024-01-10 04:13:31 +0100

    Added necessary import

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import bcrypt
 import database
+from database import DatabaseError
 
 
 app = FastAPI()

c7ebef2472cafbd179d5ac693fc6a6935acd597b
mtwarow1 <01169255@pw.edu.pl>
2024-01-10 07:06:36 +0100

    finished functionality

--- app/src/views/UserProfile.jsx
+++ app/src/views/UserProfile.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import UserData from '../components/UserData'
 import Wallets from "../components/Wallets";
+import AddNewWallet from '../components/AddNewWallet';
 
 function UserProfile () {
     return (
         <div className="BLUE_BG">
           <UserData />
+          <AddNewWallet />
           <Wallets />
         </div>
       );

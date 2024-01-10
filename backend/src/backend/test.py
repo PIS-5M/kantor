03efec2003c9a4a79764dc46ebd619465d09d6d0
Marta Sobol <marta.sobol.stud@pw.edu.pl>
2024-01-10 04:24:44 +0100

    P5K-91 add tests

--- backend/src/backend/test.py
+++ backend/src/backend/test.py
@@ -1,6 +1,7 @@
 import pytest
 from pytest_mock import mocker
 from backend.database import login
+import backend.database as db
 
 def test_login_successful(mocker):
     # Mock the cursor and execute method
@@ -36,3 +37,28 @@ def test_login_database_error(mocker):
 
     with pytest.raises(Exception, match='Database error'):
         login('test@example.com', 'password123')
+
+
+def test_get_wallet(mocker):
+    # Mock the cursor and execute method
+    fake_results = [('USD', 1, 123, 500.0, 1000.0)]
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.fetchall.return_value = fake_results
+    # Przygotowanie danych testowych
+    user_id = 1
+
+    # Wywołanie funkcji
+    result = db.get_wallet(user_id)
+
+    assert result == fake_results
+
+
+def test_get_wallet_error(mocker):
+    # Mock the cursor and execute method to raise an exception
+    mocker.patch('backend.database.mysql.connector.connect')
+    mock_cursor = mocker.patch('backend.database.mysql.connector.connect.return_value.cursor.return_value')
+    mock_cursor.execute.side_effect = Exception('Database error')
+
+    with pytest.raises(Exception, match='Database error'):
+        db.get_wallet(1)

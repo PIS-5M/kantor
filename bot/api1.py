f44093df9bc204a4e41826a730eda9600000efa0
Maria Polakowska <01169227@pw.edu.pl>
2024-01-10 05:35:09 +0100

    P5K-45 add scene 1, 2, 3, 4

--- bot/api1.py
+++ bot/api1.py
@@ -7,24 +7,20 @@ app = FastAPI()
 def get_all_currency():
     url = "http://localhost:8000/registration"
     headers = {"Content-Type": "application/json"}
-
     try:
         response = httpx.post(url, headers=headers)
-
         if response.status_code == 200:
             data = response.json()
             print(data)
             return {"message": "Data from Microservice 2", "data": data}
         else:
             return {"error": response}
-
     except httpx.HTTPError as e:
         return {"error": f"HTTP error occurred: {e}"}
 
 def get_uncompleted_offers():
     url = "http://localhost:8000/get_uncompleted_offers"
     headers = {"Content-Type": "application/json"}
-
     try:
         response = httpx.post(url, headers=headers)
         if response.status_code == 200:
@@ -32,6 +28,42 @@ def get_uncompleted_offers():
             return data['offers']
         else:
             return {"error": response}
-
     except httpx.HTTPError as e:
         return {"error": f"HTTP error occurred: {e}"}
+
+def show_wallet(user_id):
+    url = "http://localhost:8000/show_wallet"
+    headers = {"Content-Type": "application/json"}
+    body = {'user_id': user_id}
+
+    try:
+        response = httpx.post(url, json=body, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['wallet']
+        else:
+            return {"error": response}
+    except httpx.HTTPError as e:
+        return {"error": f"HTTP error occurred: {e}"}
+
+
+def add_offer(user_id, selled_currency_id, value, wanted_currency_id, exchange_rate):
+    url = "http://localhost:8000/add_offer"
+    headers = {"Content-Type": "application/json"}
+    body = {
+        'user_id': user_id,
+        'selled_currency_id': selled_currency_id,
+        'value': value,
+        'wanted_currency_id': wanted_currency_id,
+        'exchange_rate': exchange_rate
+        }
+    try:
+        response = httpx.post(url, json=body, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['matches']
+        else:
+            return {"error": response}
+    except httpx.HTTPError as e:
+        return {"error": f"HTTP error occurred: {e}"}
+
--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,19 @@ import api1 as ap
 import time
 import os
 
+
+bots = {
+    1 : 'bot 1',
+    2 : 'bot 2',
+    3 : 'bot 3'
+    }
+
+currency = {
+    1: 'USD',
+    2: 'EUR'
+}
+
+
 def clear():
     os.system('cls')
     # os.system('clear')
@@ -10,6 +23,13 @@ def sleep():
     time.sleep(2)
 
 def main():
+    # introduction()
+    # scene1()
+    # scene2()
+    scene3()
+    scene4()
+
+def introduction():
     clear()
     print('\n\nWitamy w kantorze 5MONET.')
     sleep()
@@ -17,15 +37,74 @@ def main():
     sleep()
     print('Mamy 3 użytkowników, którzy będą testować różne scenariusze')
     sleep()
+
+def scene1():
     clear()
     print('\nScenariusz 1')
     sleep()
     print('opis')
-    clear()
-    scene1()
-
-def scene1():
     sleep()
+    print_offers()
+    wallet()
+    print('Bot 1 wystawia ofertę na 50 dolarów za euro w cenie 2.0')
+    sleep()
+    add_offer(1, 1, 50.0, 2, 2.0)
+    print('Bot 2 wystawia ofertę na 100 euro za dolary w cenie 0.5')
+    sleep()
+    add_offer(2, 2, 100.0, 1, 0.5)
+
+def scene2():
+    clear()
+    print('\nScenariusz 2')
+    sleep()
+    print('opis')
+    sleep()
+    print_offers()
+    wallet()
+    print('Bot 1 wystawia ofertę na 50 dolarów za euro w cenie 20')
+    sleep()
+    add_offer(1, 1, 50.0, 2, 20.0)
+    print('Bot 2 wystawia ofertę na 100 euro za dolary w cenie 20')
+    sleep()
+    add_offer(2, 2, 100.0, 1, 20)
+
+
+def scene3():
+    clear()
+    print('\nScenariusz 3')
+    sleep()
+    print('opis')
+    sleep()
+    print_offers()
+    wallet()
+    print('Bot 1 wystawia ofertę na 50 dolarów za euro w cenie 2.0')
+    sleep()
+    add_offer(1, 1, 50.0, 2, 2.0)
+    print('Bot 2 wystawia ofertę na 40 dolarów za euro w cenie 1.98')
+    sleep()
+    add_offer(2, 1, 40.0, 2, 1.98)
+    print('Bot 3 wystawia ofertę na 200 euro za dolary w cenie 0.5')
+    sleep()
+    add_offer(3, 2, 200.0, 1, 0.5)
+
+def scene4():
+    clear()
+    print('\nScenariusz 4')
+    sleep()
+    print('opis')
+    sleep()
+    print_offers()
+    wallet()
+    print('Bot 1 wystawia ofertę na 50 dolarów za euro w cenie 1.97')
+    sleep()
+    add_offer(1, 1, 50.0, 2, 1.97)
+    print('Bot 2 wystawia ofertę na 40.67 dolarów za euro w cenie 1.97')
+    sleep()
+    add_offer(2, 1, 40.67, 2, 1.97)
+    print('Bot 3 wystawia ofertę na 200 euro za dolary w cenie 0.5')
+    sleep()
+    add_offer(3, 2, 200.0, 1, 0.5)
+
 
 
 
@@ -33,15 +112,36 @@ def print_offers():
     print('Dostępne oferty:')
     sleep()
     offers = ap.get_uncompleted_offers()
-    for offer in offers:
-        print(offer)
+    print('sprzedawane\tposzukiwane\tkurs')
+    if offers:
+        for offer in offers:
+            print(f'{offer[8]} {currency[offer[4]]}\t{currency[offer[6]]}\t\t{offer[7]}')
+            sleep()
+    else:
+        print('Brak dostępnych ofert.')
         sleep()
 
 def wallet():
     print('Stan portfeli:')
     sleep()
+    print('Waluta\t\t\tbot\tdostępne w portfelu\tw ofertach')
     for i in range(1, 4):
-        pass
+        wallets = ap.show_wallet(i)
+        print(f'{wallets[0][0]}\t{bots[wallets[0][2]]}\t{wallets[0][3]}\t\t\t{wallets[0][4]}')
+        print(f'{wallets[1][0]}\t\t\t{bots[wallets[1][2]]}\t{wallets[1][3]}\t\t\t{wallets[1][4]}')
+        sleep()
+
+
+def add_offer(user_id, selled_currency_id, value, wanted_currency_id, exchange_rate):
+    match_list = ap.add_offer(user_id, selled_currency_id, value, wanted_currency_id, exchange_rate)
+    if not match_list:
+        print('Brak pasujących offert.')
+    for matching in match_list:
+        print(f'{bots[user_id]} za {matching[0]} {currency[selled_currency_id]} dostał {matching[1]} {currency[wanted_currency_id]}')
+        sleep()
+    print_offers()
+    wallet()
+
 
 
 if __name__ == "__main__":

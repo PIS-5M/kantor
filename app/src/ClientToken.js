8b21269f678e64576e073f3dd89aa4614ba3509d
WizardWillGive <133001075+WizardWillGive@users.noreply.github.com>
2024-01-10 05:26:12 +0100

    Unmocked ClientToken.js

--- app/src/ClientToken.js
+++ app/src/ClientToken.js
@@ -1,39 +1,39 @@
-// export default function clientToken() {
-//   const login = (id) => {
-//     sessionStorage.setItem("userId", JSON.stringify(id));
-//   };
-
-//   const logout = () => {
-//     sessionStorage.removeItem("userId");
-//   };
-
-//   const userId = () => {
-//     const tokenString = sessionStorage.getItem("userId");
-//     if (tokenString) {
-//       const userData = JSON.parse(tokenString);
-//       return userData;
-//     }
-//     return null;
-//   };
-
-//   return { login, logout, userId };
-// }
-
 export default function clientToken() {
-  // Mock login function
   const login = (id) => {
-    console.log(`Mock login with id: ${id}`);
+    sessionStorage.setItem("userId", JSON.stringify(id));
   };
 
-  // Mock logout function
   const logout = () => {
-    console.log("Mock logout");
+    sessionStorage.removeItem("userId");
   };
 
-  // Mock userId function to always return 1
   const userId = () => {
-    return 1;
+    const tokenString = sessionStorage.getItem("userId");
+    if (tokenString) {
+      const userData = JSON.parse(tokenString);
+      return userData;
+    }
+    return null;
   };
 
   return { login, logout, userId };
 }
+
+// export default function clientToken() {
+//   // Mock login function
+//   const login = (id) => {
+//     console.log(`Mock login with id: ${id}`);
+//   };
+
+//   // Mock logout function
+//   const logout = () => {
+//     console.log("Mock logout");
+//   };
+
+//   // Mock userId function to always return 1
+//   const userId = () => {
+//     return 1;
+//   };
+
+//   return { login, logout, userId };
+// }
